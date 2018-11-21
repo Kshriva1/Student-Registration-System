cc7022048d014cf094f11da5de81ebfe4e09acb7
Sean <sean@eponascience.com>
2018-11-21 03:33:18 -0500

    Did query 4 please test

--- query4.sql
+++ query4.sql
@@ -1,33 +1,41 @@
-Create or replace procedure get_prerequisites(v_dept_code in courses.dept_code%type,v_course# in courses.course#%type,error_message out varchar2,r_cursor out sys_refcursor) is
-v_found_dept_code_course# Number;
-cursor prereq_cursor is
-select dept_code,course# from prerequisites
-where pre_dept_code = v_dept_code
-and pre_course# = v_course#;
+set serveroutput on;
+create or replace procedure show_prereqs(
+	dCode in Courses.dept_code%type, 
+	cNum in Courses.course#%type) IS
 
-prereq_record prereq_cursor%rowtype;
+	--local definitions
+	temp_dCode Courses.dept_code%type;
+	temp_cNum Courses.course#%type;
+	l_dept_code Courses.dept_code%type;
+	l_course# Courses.course#%type;
+
+	CURSOR c_prereqs IS
+		SELECT pre_dept_code, pre_course# FROM Prerequisites WHERE dept_code = dCode and course# = cNum;
 
 BEGIN
-SELECT count(*) into v_found_dept_code_course# FROM Courses WHERE
-dept_code = v_dept_code and course# = v_course#;
-if (v_found_dept_code_course# = 0) THEN
-        error_message := v_dept_code||','||v_course#||' is invalid';
+	SELECT dept_code, course# INTO
+	temp_dCode, temp_cNum FROM Courses
+	WHERE dept_code = dCode and course# = cNum;
 
-else
-        insert into temp_prerequisites select dept_code,course# from
-prerequisites where pre_dept_code = v_dept_code and pre_course# =
-v_course#;
-open prereq_cursor;
-loop
-        fetch prereq_cursor into prereq_record;
-        exit when prereq_cursor%notfound;
+	OPEN c_prereqs;
+	LOOP
+	FETCH c_prereqs into l_dept_code, l_course#;
+		EXIT WHEN c_prereqs%notfound;
+		DBMS_output.put_line(l_dept_code || l_course#);
+		DBMS_output.put_line('----------------------');
+		show_prereqs(l_dept_code, l_course#);
+		DBMS_output.put_line('----------------------');	
+	
+	END LOOP;
+	CLOSE c_prereqs;
+	Return;	
 
-get_prerequisites(prereq_record.dept_code,prereq_record.course#,error_message,r_cursor);
-end loop;
-open r_cursor for select * from temp_prerequisites;
-close prereq_cursor;
-end if;
-end;
+
+EXCEPTION WHEN NO_DATA_FOUND THEN
+	IF temp_dCode is null and temp_cNum is null then	 
+		DBMS_output.put_line(dCode || cNum || ' does not exist');
+	END IF;	
+
+END;
 /
 show errors;
-
