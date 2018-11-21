372ae1ee7afbd57c87514137f1a8a566a3eec528
Kundan Shrivastav <kundan0395@gmail.com>
2018-11-21 16:14:57 -0500

    Update trigger7.sql

--- trigger7.sql
+++ trigger7.sql
@@ -1,11 +1,20 @@
-create or replace trigger del_student
+create or replace trigger STUDENT_DELETE
 AFTER DELETE ON Students
 FOR EACH ROW
 DECLARE
+  user_log varchar2(20);
+  operation_log varchar2(20) default 'delete';
+  B#_log enrollments.B#%type;
+  table_name_log nvarchar2(20) default 'students';
+  id_log Number;
 BEGIN
-	DELETE FROM Enrollments WHERE B# = :old.B#;
-	--DELETE FROM TAs WHERE B# = :old.B#;
-	--UPDATE Classes SET TA_B# = NULL WHERE TA_B# = :old.B#;
+  B#_log := :old.B#;
+  id_log := logs_seq.nextval;
+  select user into user_log from dual;
+  Insert into logs
+  values(id_log,user_log,sysdate,table_name_log,operation_log,B#_log);
+  DELETE FROM Enrollments WHERE B# = B#_log;
 END;
 /
 show errors;
+
