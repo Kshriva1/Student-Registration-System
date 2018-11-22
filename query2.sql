a7e0b93df314ae27af21f47e13b1e52bcf720b77
Kundan Shrivastav <kundan0395@gmail.com>
2018-11-21 23:31:25 -0500

    Update query2.sql

--- query2.sql
+++ query2.sql
@@ -1,21 +1,80 @@
-set serveroutput on
-CREATE OR REPLACE PROCEDURE show_students IS
-        cursor c1 is
-        select * from students;
-        c1_rec c1%rowtype;
-begin
-        if(not c1%isopen) then
-                open c1;
-        end if;
+/*  student table */
 
-        fetch c1 into c1_rec;
-                while c1%found loop
-                dbms_output.put_line(c1_rec.B#||','||c1_rec.first_name||','||c1_rec.last_name);
-                fetch c1 into c1_rec;
-                end loop;
-        close c1;
+create or replace procedure show_students(student_cursor out
+sys_refcursor) AS
+BEGIN
+        open student_cursor for
+        select * from students;
+END;
+/
+show errors;
+
+/* courses table */
+
+create or replace procedure show_courses(student_cursor out
+sys_refcursor) AS
+BEGIN
+        open student_cursor for
+        SELECT * FROM COURSES;
+END;
+/
+show errors;
+
+
+/* TAs table */
+
+create or replace procedure show_TAs(student_cursor out sys_refcursor)
+AS
+BEGIN
+        open student_cursor for
+        select * from TAs;
 end;
 /
-
 show errors;
+
+
+/* classes table */
+
+Create or replace procedure show_classes(student_cursor out
+sys_refcursor) as
+BEGIN
+        open student_cursor for
+        select * from classes;
+END;
+/
+
+
+/* enrollments table */
+
+create or replace procedure get_enrollments(student_cursor out
+sys_refcursor)
+AS
+BEGIN
+        open student_cursor for
+        select * from enrollments;
+END;
+/
+
+
+/* prerequisites table */
+
+create or replace procedure get_prerequisites(student_cursor out
+sys_refcursor) AS
+BEGIN
+        open student_cursor for
+        select * from prerequisites;
+END;
+/
+
+
+
+/* logs table */
+
+create or replace procedure get_logs(student_cursor out sys_refcursor)
+AS
+BEGIN
+        open student_cursor for
+        select * from logs;
+END;
+/
 
